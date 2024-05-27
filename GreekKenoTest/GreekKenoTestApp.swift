b537d2c80bcd78196a96210ffefb1fb70f95ba5f
Marko Djukic <m.djukic@levi9.com>
2024-05-27 19:11:03 +0200

    Initial Commit

---
+++ GreekKenoTest/GreekKenoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreekKenoTestApp.swift
+//  GreekKenoTest
+//
+//  Created by Marko Djukic on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct GreekKenoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
